606557643efe635d22bd99cc2eab6b170bf8d30e
twinther <tommy@winther.nu>
2012-04-24 19:44:21 +0200

    move timebar every 3 seconds

--- gui.py
+++ gui.py
@@ -123,7 +123,7 @@ class TimeBarMover(threading.Thread):
     def run(self):
         while not xbmc.abortRequested and not self.gui.isClosing:
             self.gui.updateTimeBar()
-            xbmc.sleep(10000)
+            xbmc.sleep(3000)
 
 class Point(object):
     def __init__(self):
